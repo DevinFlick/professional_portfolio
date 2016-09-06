7ed4d2369c4eaccf1fab2be2faa441351f60c034
DevinFlick <DevinJFlick@gmail.com>
2016-09-05 21:30:52 -0400

    trying to impliment mailer functionality

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var server = express();
 var bodyParser = require('body-parser');
+var mailer = require('./mailer.js');
 
 var port = process.env.PORT || 8080;
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended: true}));
 server.use(express.static(__dirname + '/public'));
+server.use(mailer);
 
 server.get('/', function (req, res){
   res.sendFile('index.html', {root: __dirname + '/public/html'});
---
+++ public/js/controllers/MailController.js
@@ -0,0 +1,18 @@
+(function() {
+  angular.module('ngPortPage')
+          .controller('MailController', MailController);
+
+  MailController.$inject = ['$scope', '$http'];
+
+  function MailController($scope, $http){
+    $scope.sendMail = sendMail;
+    var baseUrl = 'https://devinflick-portfolio.herokuapp.com/send';
+
+    function sendMail(msg){
+      return $http.post(baseUrl, msg)
+                  .then(function(response){
+                    console.log('sent email', response);
+                  });
+    }
+  }
+}());
